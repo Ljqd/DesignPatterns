409186dd5f29c6e3b54ac6b30d69d335b3572b40
Molochynskyi Yehor <molochynskyi.e@gmail.com>
2023-04-12 20:20:46 +0300

    Add test for Air and Sea logistics

--- 1. Factory method/1. Traffic simulator/main.cpp
+++ 1. Factory method/1. Traffic simulator/main.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
-
 #include <memory>
+#include <vector>
+
 #include "src/TrafficSimulator.h"
 
 int main()
 {
     std::unique_ptr<TrafficSimulator> simulator = std::unique_ptr<TrafficSimulator>();
-    float cost = simulator->simulate();
-    std::cout << "I'm a traffic simulator!\tSo, the total cost is: " << cost;
+    std::vector<float> costs = simulator->simulate();
+    std::cout << "I'm a traffic simulator!\tSo, the total cost 1 is: " << costs[0] << std::endl;
+    std::cout << "I'm a traffic simulator!\tSo, the total cost 2 is: " << costs[1] << std::endl;
+    std::cout << "I'm a traffic simulator!\tSo, the total cost 3 is: " << costs[2] << std::endl;
 }
--- 1. Factory method/1. Traffic simulator/src/TrafficSimulator.h
+++ 1. Factory method/1. Traffic simulator/src/TrafficSimulator.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <memory>
+#include <vector>
 
 #include "Logistics.h"
 #include "RoadLogistics.h"
@@ -10,10 +11,21 @@
 class TrafficSimulator
 {
 public:
-    float simulate() {
+    std::vector<float> simulate() {
+        std::vector<float> costs = {};
+
         std::unique_ptr<Logistics> logistic = std::make_unique<RoadLogistics>();
         float cost = logistic->planDelivery(2.0f, 500, 20);
+        costs.push_back(cost);
 
-        return cost;
+        logistic = std::make_unique<AirLogistics>();
+        cost = logistic->planDelivery(2.0f, 300, 20);
+        costs.push_back(cost);
+
+        logistic = std::make_unique<SeaLogistics>();
+        cost = logistic->planDelivery(2.0f, 100, 20);
+        costs.push_back(cost);
+
+        return costs;
     }
 };
