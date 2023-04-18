c1455a47562979881fd65bfbd0765f069e510568
Molochynskyi Yehor <molochynskyi.e@gmail.com>
2023-04-18 19:52:51 +0300

    Update main.cpp

--- 4. Prototype/2. Automobile plant/main.cpp
+++ 4. Prototype/2. Automobile plant/main.cpp
@@ -9,28 +9,30 @@ int main()
 
     // Hatchback
     std::unique_ptr<CarModule::Car> standardCar1 = director.getStandardHatchback();
-    std::unique_ptr<CarModule::Car> premiumCar1 = director.getPremiumHatchback();
-    std::unique_ptr<CarModule::Car> electricCar1 = director.getElectricHatchback();
+    std::unique_ptr<CarModule::Car> premiumCar1 = director.getPremiumSedan();
+    std::unique_ptr<CarModule::Car> electricCar1 = director.getElectricSuv();
+
+    // clone
+    std::unique_ptr<CarModule::Car> standardCar2 = standardCar1->clone();
+    std::unique_ptr<CarModule::Car> premiumCar2 = premiumCar1->clone();
+    std::unique_ptr<CarModule::Car> electricCar2 = electricCar1->clone();
+
+    std::cout << "Original:\n";
 
     std::cout << "1 (standard)." + standardCar1->getString();
-    std::cout << "1 (premium)." + premiumCar1->getString();
-    std::cout << "1 (electric)." + electricCar1->getString();
+    std::cout << "2 (premium)." + premiumCar1->getString();
+    std::cout << "3 (electric)." + electricCar1->getString();
 
-    // Sedan
-    std::unique_ptr<CarModule::Car> standardCar2 = director.getStandardSedan();
-    std::unique_ptr<CarModule::Car> premiumCar2 = director.getPremiumSedan();
-    std::unique_ptr<CarModule::Car> electricCar2 = director.getElectricSedan();
+    std::cout << "\nCloned:\n";
 
-    std::cout << "2 (standard)." + standardCar2->getString();
-    std::cout << "2 (premium)." + premiumCar2->getString();
-    std::cout << "2 (electric)." + electricCar2->getString();
+    // now let's perform some modifications:
+    standardCar2->getEngine().setPower(9999);
+    premiumCar2->getTransmission().setGears(2);
+    electricCar2->getWheels().setDiameter(1000);
 
-    // Suv
-    std::unique_ptr<CarModule::Car> standardCar3 = director.getStandardSuv();
-    std::unique_ptr<CarModule::Car> premiumCar3 = director.getPremiumSuv();
-    std::unique_ptr<CarModule::Car> electricCar3 = director.getElectricSuv();
 
-    std::cout << "3 (standard)." + standardCar3->getString();
-    std::cout << "3 (premium)." + premiumCar3->getString();
-    std::cout << "3 (electric)." + electricCar3->getString();
+    std::cout << "1 (standard-cloned)." + standardCar2->getString();
+    std::cout << "2 (premium-cloned)." + premiumCar2->getString();
+    std::cout << "3 (electric-cloned)." + electricCar2->getString();
+
 }
