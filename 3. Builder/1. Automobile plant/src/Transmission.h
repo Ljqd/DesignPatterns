30aef87c60f86789f2a7f49b13d4be7c22e39cbb
Molochynskyi Yehor <molochynskyi.e@gmail.com>
2023-04-18 13:04:32 +0300

    Transmission: add getString()

--- 3. Builder/1. Automobile plant/src/Transmission.h
+++ 3. Builder/1. Automobile plant/src/Transmission.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <string>
+
 namespace CarModule
 {
     class Transmission
@@ -7,6 +9,10 @@ namespace CarModule
     public:
         virtual void setGears(size_t gears) = 0;
         virtual size_t getGears() const = 0;
+
+        std::string getString() const {
+            return "Transmission: gears = " + std::to_string(gears) + ";";
+        }
 
     protected:
         size_t gears;
