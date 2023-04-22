4114822134cc0e992f9bfae079a2e31197034f4a
Molochynskyi Yehor <molochynskyi.e@gmail.com>
2023-04-22 12:43:05 +0300

    Update main.cpp

--- 11. Flyweight/1. Automaker/main.cpp
+++ 11. Flyweight/1. Automaker/main.cpp
@@ -9,29 +9,8 @@ int main()
 
     // Hatchback
     std::shared_ptr<CarModule::Car> standardCar1 = director->getStandardHatchback();
-    std::shared_ptr<CarModule::Car> premiumCar1 = director->getPremiumSedan();
-    std::shared_ptr<CarModule::Car> electricCar1 = director->getElectricSuv();
+    std::shared_ptr<CarModule::Car> standardCar2 = director->getStandardHatchback();
 
-    // clone
-    std::shared_ptr<CarModule::Car> standardCar2 = standardCar1->clone();
-    std::shared_ptr<CarModule::Car> premiumCar2 = premiumCar1->clone();
-    std::shared_ptr<CarModule::Car> electricCar2 = electricCar1->clone();
-
-    // now let's perform some modifications:
-    standardCar2->getEngine().setPower(9999);
-    premiumCar2->getTransmission().setGears(2);
-    electricCar2->getWheels().setDiameter(1000);
-
-    std::cout << "\nCloned:\n";
-
-    std::cout << "1 (standard-cloned)." + standardCar2->getString();
-    std::cout << "2 (premium-cloned)." + premiumCar2->getString();
-    std::cout << "3 (electric-cloned)." + electricCar2->getString();
-
-    std::cout << "Original:\n";
-
-    std::cout << "1 (standard)." + standardCar1->getString();
-    std::cout << "2 (premium)." + premiumCar1->getString();
-    std::cout << "3 (electric)." + electricCar1->getString();
+    std::cout << ((*standardCar1) == (*standardCar2)) << std::endl;
 
 }
